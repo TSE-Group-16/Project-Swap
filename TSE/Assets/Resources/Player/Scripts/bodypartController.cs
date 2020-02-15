5c205438b8ef6e5119b839aa12879df1466a9acc
Scott Brown <scurry99@hotmail.co.uk>
2020-02-15 14:18:52 +0000

    Updated level with player and comments on code

--- TSE/Assets/Resources/Player/Scripts/bodypartController.cs
+++ TSE/Assets/Resources/Player/Scripts/bodypartController.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//interfaces for bodyparts
 
 public interface IBodypart
 {
     string GetBodyType();
+}
+
+
+public interface ILegpart
+{
+    bool canJump();
 }
---
+++ TSE/Assets/Resources/Player/Scripts/groundCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered: " + other.tag);
+        if(other.tag == "ground")
+        {
+            this.transform.parent.transform.parent.transform.parent.GetComponent<playerController>().isGrounded = true;
+            Debug.Log("grounded");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.transform.parent.transform.parent.transform.parent.GetComponent<playerController>().isGrounded = false;
+    }
+}
