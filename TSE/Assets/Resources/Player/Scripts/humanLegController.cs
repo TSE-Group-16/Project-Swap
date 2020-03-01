1845bed19e6e5f8236b99fd847f587238d971022
x-SonZ <60305686+x-SonZ@users.noreply.github.com>
2020-03-01 20:15:33 +0000

    Nearly working Jumping

--- TSE/Assets/Resources/Player/Scripts/humanLegController.cs
+++ TSE/Assets/Resources/Player/Scripts/humanLegController.cs
@@ -7,16 +7,19 @@ public class humanLegController : MonoBehaviour, IBodypart, ILegpart
     float moveSpeed;
     public bool canJumpBool = true;
 
+    public static playerController PC;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = GameObject.Find("Player");
+        PC = player.GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("leg controller bool " + PC.isGrounded);
     }
 
     public string GetBodyType()
@@ -28,4 +31,16 @@ public class humanLegController : MonoBehaviour, IBodypart, ILegpart
     {
         return canJumpBool;
     }
+
+    public void OnTriggerEnter(Collider legsTagger)
+    {
+        Debug.Log("inside on trigger stay");
+        if (legsTagger.gameObject.CompareTag("ground"))
+        {
+            Debug.Log("ground touched");
+            PC.isGrounded = true;
+            Debug.Log(PC.isGrounded);
+        }
+    }
+
 }
