5209cda30132993197c50b41827bdd2701c1d990
AppSmith <tobysmith459@gmail.com>
2020-04-18 22:14:46 +0100

    vent grate and scripts

---
+++ TSE/Assets/ventGrate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ventGrate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && inRange == true)
+        {
+            Debug.Log("Grate removed");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player" || other.transform.tag == "bodypart")
+            inRange = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player" || other.transform.tag == "bodypart")
+            inRange = false;
+    }
+}
