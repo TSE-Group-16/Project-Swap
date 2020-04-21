d1f251dec54096352140fcb57efe033d45462d87
AppSmith <tobysmith459@gmail.com>
2020-04-21 23:34:43 +0100

    Added UI for tutorial

--- TSE/Assets/Resources/Player/Scripts/grateController.cs
+++ TSE/Assets/Resources/Player/Scripts/grateController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class grateController : MonoBehaviour
 {
     [SerializeField]
-    GameObject player;
+    public GameObject player;
     playerController PC;
+    public GameObject tutorial;
+    tutorialController TC;
+
     // Start is called before the first frame update
     void Start()
     {
         PC = player.GetComponent<playerController>();
+        TC = tutorial.GetComponent<tutorialController>();
     }
 
     // Update is called once per frame
@@ -32,6 +36,18 @@ public class grateController : MonoBehaviour
         else
         {
             this.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        if (PC.getCurLeg() == null)
+        {
+            TC.ShowGrateHelp();
+        }
+        else if(PC.getCurLeg().name != "Tracks")
+        {
+            TC.ShowGrateHelp();
         }
     }
 }
